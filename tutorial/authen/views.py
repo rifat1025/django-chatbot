d2f671716f4942d883e02d0d2427dbccccb97ef9
rifat1025 <rifatsarkercse@gmail.com>
2026-08-23 16:35:47 +0600

    similar name

--- tutorial/authen/views.py
+++ tutorial/authen/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import User
 from .serializers import UserSerializer
+from rest_framework import status
 from rest_framework.views import APIView
 
 # for test my apps
@@ -26,6 +27,33 @@ class User_lists(APIView):
         users  = User.objects.all()
         serializer = UserSerializer(users,many=True)
         return Response (serializer.data)
+
+    def post(self,request):
+        # get data from userform
+        name = request.data.get('name')
+
+        # condition match the same name 
+        if User.objects.filter(name = name ).exists():
+            return Response(
+            {"error": "Name already exists"},
+            status=status.HTTP_400_BAD_REQUEST
+            )
+
+        serializer = UserSerializer(data = request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+
+            )
+        else:
+            return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+            )
+
 
 
 
